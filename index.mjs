08683ec5c4bd36719f4ffa01577ab3a0f7dfffaa
mariashapo <mart99maria@gmail.com>
2024-03-06 10:45:38 +0000

    Ensure the points are also scaled

--- index.mjs
+++ index.mjs
@@ -73,8 +73,6 @@ export async function handler(event) {
                 // Calculate the scale factor based on the number of days left in the month
                 const total_days_in_the_month = challenge_end_date.getDate();
                 const todays_day = challenge_start_date.getDate();
-                console.log("Total days in the month:", total_days_in_the_month);
-                console.log("Today's day:", todays_day);
                 scaleFactor = (total_days_in_the_month - todays_day) / total_days_in_the_month;
             } else {
                 // Add duration days to the start date
@@ -84,14 +82,14 @@ export async function handler(event) {
             }
 
             // Apply scaling to target_meters if duration is -1 to account for the remaining days in the month
-            console.log("Scale factor:", scaleFactor);
-            console.log("Targer meters before:", target_meters);
+            //console.log("Scale factor:", scaleFactor);
             if(template_data.duration === -1) {
                 target_meters = target_meters * scaleFactor;
+                points = points * scaleFactor;
                 // Ensure target_meters is rounded off as needed
                 target_meters = Math.round(target_meters / 10) * 10;
+                points = Math.round(points * 10) / 10;
             }
-            console.log("Targer meters after:", target_meters);
 
             if (challenge_end_date.getMonth() !== challenge_start_date.getMonth()) {
                 console.log("Challenge end date falls into the next month. Skipping this challenge.");
